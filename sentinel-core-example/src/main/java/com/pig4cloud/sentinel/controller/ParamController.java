a661dd255b310c509b5c2b952bb5979f2a06aa67
冷冷 <wangiegie@gmail.com>
2019-12-06 17:45:37 +0800

    sentinel 热点参数限流功能

---
+++ sentinel-core-example/src/main/java/com/pig4cloud/sentinel/controller/ParamController.java
@@ -0,0 +1,41 @@
+/*
+ *    Copyright (c) 2018-2025, lengleng All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lengleng (wangiegie@gmail.com)
+ */
+
+package com.pig4cloud.sentinel.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lengleng
+ * @date 2019-12-05
+ */
+@RestController
+public class ParamController {
+    @GetMapping("/param")
+    @SentinelResource
+    public String param(String type) {
+        return "success";
+    }
+
+    public String exHandler(String type, BlockException ex) {
+        return "exHandler error";
+    }
+
+}
